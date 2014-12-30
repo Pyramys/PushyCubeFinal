b023080459981abcda957031a33dbe217d7d6ee6
Redeamed <kdavis928@yahoo.com>
2014-12-30 13:19:24 -0700

    Menu Scene Animations
    Zombie and wizard now run idle animation and occasionally "wave" at the
    player.

---
+++ Assets/Scripts/WaveScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveScript : MonoBehaviour {
+
+    Animator anim;
+    public float chance = 0.0f;
+    public bool Wave = false;
+    public bool roll = true;
+	// Use this for initialization
+	void Start () 
+    {
+        anim = GetComponent<Animator>();
+        InvokeRepeating("ChanceWave", 0, 3);
+	}
+	
+	// Update is called once per frame
+	void ChanceWave () {
+        if (roll)
+        {
+            chance = Random.Range(0, 99);
+            if (chance > 70)
+            {
+                anim.SetTrigger("Wave");
+            }
+        }
+	}
+}
